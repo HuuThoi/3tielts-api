8977a80dcae6609b7626fecbfabe440e108269d6
= <=>
2020-06-07 16:44:13 +0700

    comment get all in user route

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -10,6 +10,6 @@ router.get("/:limit/:offset", controller.findAll);
 // router.get("/:name", controller.fin)
 router.post("/register", controller.register);
 // router.put('/update', controller.);
-router.get("/all", controller.getList);
+// router.get("/all", controller.getList);
 
 module.exports = router;
