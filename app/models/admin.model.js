f61a5da0aadbaf9acd06f3dd09b7e48a715fdaf6
HuuThoi <thoi.huynh@nois.com>
2020-06-11 07:59:46 +0700

    change required field

--- app/models/admin.model.js
+++ app/models/admin.model.js
@@ -1,23 +1,16 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const saltRounds = 10;
-
-const AdminSchema = mongoose.Schema({
-  userID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const AdminSchema = mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-},{
-  timestamps: true
-});
-
-AdminSchema.methods.setPassword = function (password) {
-  this.passwordHash = bcrypt.hashSync(password, saltRounds);
-};
-
-AdminSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.passwordHash);
-};
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Admin", AdminSchema);
